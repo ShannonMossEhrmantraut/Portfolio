0b7b852b6f74f8b40b1eca197a46328ae512bc38
Shannon Moss- Ehrmantraut <sehrmantraut5@gmail.com>
2022-10-05 20:50:17 -0400

    Javascript-Cart Only

---
+++ ShoppingCart-FCC/src/data.js
@@ -0,0 +1,58 @@
+let shopItemsData = [
+   {
+      id: "r1",
+      name: "Casual Shirt",
+      price: 45,
+      desc: "lorem7 ",
+      img: "images/img-1.jpg"
+   },
+   {
+      id: "r2",
+      name: "Office Shirt",
+      price: 100,
+      desc: "lorem7 ",
+      img: "images/img-2.jpg"
+   },
+   {
+      id: "r3",
+      name: "Comfy Shirt",
+      price: 25,
+      desc: "lorem7 ",
+      img: "images/img-3.jpg"
+   },
+   {
+      id: "r4",
+      name: "Men's Suit",
+      price: 300,
+      desc: "lorem7 ",
+      img: "images/img-4.jpg"
+   },
+   {
+      id: "r5",
+      name: "Comfy Shirt",
+      price: 25,
+      desc: "lorem7 ",
+      img: "images/img-5.jpg"
+   },
+   {
+      id: "r6",
+      name: "Men's Suit",
+      price: 300,
+      desc: "lorem7 ",
+      img: "images/img-6.jpg"
+   },
+   {
+      id: "r7",
+      name: "Comfy Shirt",
+      price: 25,
+      desc: "lorem7 ",
+      img: "images/img-7.jpg"
+   },
+   {
+      id: "r8",
+      name: "Men's Suit",
+      price: 300,
+      desc: "lorem7 ",
+      img: "images/img-4.jpg"
+   }
+   ];
